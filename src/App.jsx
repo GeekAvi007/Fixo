8230f2824ec1595ec9afec680be1872eddacc8c1
Avishek <avishekmukherje007@gmail.com>
2025-02-05 10:46:25 +0530

    Project Initiated

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='justify-center text-white'>Fixo- Your Drive's Solution right at your Doorstep!</div>
+    </>
+  )
+}
+
+export default App
